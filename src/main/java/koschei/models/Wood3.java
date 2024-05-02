810289141488a402c83e63a2466f0bb4325dbcd1
Ali <x1q8qkijnf@protonmail.com>
2024-05-03 02:36:45 +0300

    fff

--- src/main/java/koschei/models/Wood3.java
+++ src/main/java/koschei/models/Wood3.java
@@ -1,14 +1,12 @@
 package koschei.models;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("wood")
 public class Wood3 {
 
     private final Rabbit4 rabbit;
 
-    @Autowired
     public Wood3(Rabbit4 rabbit) {
         this.rabbit = rabbit;
     }
