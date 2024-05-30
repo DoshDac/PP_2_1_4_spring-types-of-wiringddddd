c7ff35e671971fc20dba748c85436840d707f547
Ali <x1q8qkijnf@protonmail.com>
2024-05-31 02:14:10 +0300

    .

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,11 +1,9 @@
 package koschei.models;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 public class Egg6 {
+
     private final Needle7 needle;
 
-    @Autowired
     public Egg6(Needle7 needle) {
         this.needle = needle;
     }
