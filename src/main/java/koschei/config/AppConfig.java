4eff00d845b2eaa6105c429fe16f8cda3f2bf696
DoshDac <160410726+DoshDac@users.noreply.github.com>
2024-06-01 03:08:05 +0300

    Update AppConfig.java

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -9,7 +9,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages = "koschei")
 public class AppConfig {
-
+    @Bean
+    public static Egg6 getEgg(Needle7 needle7){
+        return new Egg6(needle7);
     @Bean
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
